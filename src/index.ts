54184057a5e2b38c81c401696a16ccda4fd20f24
imrany <imranmat254@gmail.com>
2023-06-26 16:51:03 +0300

    removing ejs to use public folder to hold static files

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,13 @@ config()
 
 const app =express()
 const upload=multer({dest:`${__dirname}/uploads`})
-app.set('view engine','ejs');
+// app.set('view engine','ejs');
+app.use(express.static(`${__dirname}/views`));
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors({}))
 
 //routes
-app.get("/",(req:any,res:any)=>{
-    res.render("index")
-})
-
 app.post("/upload",upload.array("files"),async(req:any,res:any)=>{
     try {
         console.log(req.body)
