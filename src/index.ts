2878e4fc37957e717304f9e9792e353ffd4ce0ab
imrany <imranmat254@gmail.com>
2023-06-26 20:15:31 +0300

    added receiver.html

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from "express"
 import { config } from "dotenv"
 import multer from "multer"
 import cors from "cors"
+import {readdir} from "fs"
 config()
 
 const app =express()
-const upload=multer({dest:`./uploads`})
+const path=`./uploads`
+const upload=multer({dest:path})
 // app.set('view engine','ejs');
 app.use(express.static(`views`));
 app.use(express.json())
@@ -17,6 +19,19 @@ app.post("/upload",upload.array("files"),async(req:any,res:any)=>{
     try {
         console.log(req.files)
         res.status(200).send({msg:"File received"})
+    } catch (error:any) {
+        res.status(505).send({error:error.message})
+    }
+})
+
+app.get("/read_file",async(req:any,res:any)=>{
+    try {
+        readdir(path,"utf8",(err:any,files)=>{
+            if(err){
+                console.log(err)
+            }
+            res.send(files)
+        })
     } catch (error:any) {
         res.status(505).send({error:error.message})
     }
