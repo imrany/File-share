b520ee490414c1a5b85563284c4f6535000e55ff
imrany <imranmat254@gmail.com>
2023-06-28 21:25:46 +0300

    using socket.io on both server and client side

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,42 @@
 import express from "express"
 import { config } from "dotenv"
-import multer from "multer"
 import cors from "cors"
-import {readdir, mkdir} from "fs"
 config()
 
+const cors_option = {
+    origin: "*",
+    methods: ["GET", "POST"]
+ }
+
 const app =express()
-const path=`./uploads`
-const storage=multer.diskStorage({
-    destination:(req:any,file:any,callback:any)=>{
-        callback(null,path)
-    },
-    filename:(req:any,file:any,callback:any)=>{
-        callback(null,file.originalname)
-    }
-})
-const upload=multer({storage:storage})
 // app.set('view engine','ejs');
 app.use(express.static(`views`));
-app.use(express.static(`uploads`));
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors({}))
 
-//routes
-app.post("/upload",upload.array("files"),async(req:any,res:any)=>{
-    try {
-        console.log(req.files)
-        res.status(200).send({msg:"File received"})
-    } catch (error:any) {
-        res.status(505).send({error:error.message})
-    }
-})
-
-app.get("/read_file",async(req:any,res:any)=>{
-    try {
-        readdir(path,"utf8",(err:any,files)=>{
-            if(err){
-                console.log(err)
-                mkdir(path,()=>{
-                    console.log(`uploads dir made`)
-                })
-            }else{
-                res.send(files)
-            }
-        })
-    } catch (error:any) {
-        res.status(505).send({error:error.message})
-    }
-})
-
 const port=process.env.PORT||8000
-app.listen(port,()=>{
+const server=app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
 })
+
+let io = require("socket.io")(server,{
+    cors: {
+        origin: "http://localhost:8000",
+        methods: ["GET", "POST"],
+        transports: ['websocket', 'polling'],
+        credentials: true
+    },
+    allowEIO3: true
+});
+
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+io.on("connection", function(socket: any) {
+    console.log("a user connected");
+    // whenever we receive a 'message' we log it out
+    socket.on("message", function(message: any) {
+      console.log(message);
+    });
+  });
+  
