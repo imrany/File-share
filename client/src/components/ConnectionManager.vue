3e943c514af8af112a04de151503c1061272bca2
imrany <imranmat254@gmail.com>
2023-07-19 21:06:59 +0300

    added socket.io-vue and some images

---
+++ client/src/components/ConnectionManager.vue
@@ -0,0 +1,23 @@
+<script>
+import { socket } from "@/socket";
+
+export default {
+  name: "ConnectionManager",
+
+  methods: {
+    connect() {
+      socket.connect();
+    },
+    disconnect() {
+      socket.disconnect();
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="">
+        <button @click="connect()">Connect</button>
+        <button @click="disconnect()">Disconnect</button>
+    </div>
+</template>
