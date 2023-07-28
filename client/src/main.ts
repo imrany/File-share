1d4a42e710ad04e2e9a2d29cc955638c632f51fe
imrany <imranmat254@gmail.com>
2023-07-28 18:33:03 +0300

    added serviceworker

--- client/src/main.ts
+++ client/src/main.ts
@@ -9,3 +9,9 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register("./sw.js")
+    .then(()=>console.log('sw registered'))
+    .catch((err)=>console.log('sw not registerd', err))
+}
