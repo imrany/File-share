6f579cbfd1c5c54240676cd3f720269cc966f2b6
imrany <imranmat254@gmail.com>
2023-07-30 14:39:09 +0300

    PWA stuff

--- client/public/sw.js
+++ client/public/sw.js
@@ -10,7 +10,7 @@
 // ]
 
 // //installing service worker
-// self.addEventListener('install', (evt:any) => {
+// self.addEventListener('install', (evt) => {
 //     evt.waitUntil(
 //         caches.open(staticCacheName).then(cache => {
 //             console.log('assests been added')
@@ -20,19 +20,19 @@
 // });
 
 // //cache limit function
-// // const limitCacheSize = (name:string, size:number) => {
-// //     caches.open(name).then((cache) => {
-// //         cache.keys().then((keys) => {
-// //             if (keys.length > size) {
-// //                 cache.delete(keys[0]
-// //                     .then(limitCacheSize(name, size)))
-// //             }
-// //         })
-// //     })
-// // }
+// const limitCacheSize = (name, size) => {
+//     caches.open(name).then((cache) => {
+//         cache.keys().then((keys) => {
+//             if (keys.length > size) {
+//                 cache.delete(keys[0]
+//                     .then(limitCacheSize(name, size)))
+//             }
+//         })
+//     })
+// }
 
 // //activate service worker.
-// self.addEventListener('activate', (evt:any) => {
+// self.addEventListener('activate', (evt) => {
 //     evt.waitUntil(
 //         caches.keys().then(keys => {
 //             return Promise.all(keys.filter(key => key !== staticCacheName && key !== dynamicCache)
@@ -43,7 +43,7 @@
 // });
 
 // //fetch event
-// self.addEventListener('fetch', (evt:any) => {
+// self.addEventListener('fetch', (evt) => {
 //     evt.respondWith(
 //         caches.match(evt.request).then(cacheRes => {
 //             return cacheRes || fetch(evt.request).then(async fetchRes => {
@@ -53,7 +53,7 @@
 //             })
 //         }).catch(() => {
 //             if (evt.request.url.indexOf('.html') > -1) {
-//                 return caches.match('/');
+//                 return caches.match('/j');
 //             }
 //         })
 //     )
--- client/src/main.ts
+++ client/src/main.ts
@@ -3,15 +3,7 @@ import router from './router'
 import App from './App.vue'
 import 'primeicons/primeicons.css';
 import './assets/main.css'
+
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
-
-//register service worker
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register("./sw.js")
-    .then(()=>console.log('sw registered'))
-    .catch((err)=>console.log('sw not registerd', err))
-}
