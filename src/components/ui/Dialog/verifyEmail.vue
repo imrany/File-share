e59980ab7cb45299c590ef2e2cb34dd78d1b1b58
imrany <imranmat254@gmail.com>
2023-08-17 21:14:25 +0300

    ui update

--- src/components/ui/Dialog/verifyEmail.vue
+++ src/components/ui/Dialog/verifyEmail.vue
@@ -24,14 +24,14 @@ async function handleVerify(e:any){
 </script>
 
 <template>
-   <dialog id="verify-dialog" class="shadow-lg rounded-md flex flex-col lg:w-[35vw] max-md:w-[80vw] max-sm:w-[75vw] h-fit text-[#0e0e0e] scale-[0.9] p-10">
+   <dialog id="verify-dialog" class="shadow-lg rounded-md flex flex-col lg:w-[35vw] max-md:w-[90vw] h-fit text-[#0e0e0e] scale-[0.9] p-10">
         <button  class="ml-[auto]" @click="dialog_close">
             <i class="icon pi pi-times text-lg hover:text-[#F45858]"></i>
         </button>
         <p class="text-center text-2xl font-semibold">Get verified</p>
         <div class="flex flex-col w-full">
             <p class="text-red-500 text-center text-xl">{{error}}</p>
-            <form class="flex flex-col items-center my-4" @submit="handleVerify">
+            <form class="flex flex-col items-center my-4 max-sm:my-2" @submit="handleVerify">
                 <input type="email" name="email" class="mt-2 border-gray-300 border-[1px] bg-white rounded-lg focus:outline-1 focus:outline-[#e9972c] w-[100%] py-2 px-4 placeholder:text-sm text-sm" placeholder="Enter your work email" required/>
                 <button :disabled="isLoading" class="mt-4 bg-[#e9972c] text-white w-fit px-5 py-2 flex text-sm h-fit  cursor-pointer rounded-[5px]" :class="wait">
                    <span>Submit</span>
--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -16,23 +16,23 @@ const open_verify_dialog=()=>{
                     <img src="/favicon.png" class="w-[33px] h-[35px]" alt="."/>
                     <p class="text-2xl ml-2">Fileshare</p>
                 </div>
-                <RouterLink to="/signin" class="text-black w-[140px] h-[50px] flex justify-center items-center bg-[#eba953]">Sign in</RouterLink>
+                <RouterLink to="/signin" class="rounded-[5px] text-black w-[140px] h-[50px] flex justify-center items-center bg-[#eba953]">Sign in</RouterLink>
             </div>
 
             <div class="flex flex-col ml-20">
-                <p class="font-bold text-7xl w-[500px] my-10" style="line-height:80px;">
-                    Increasing the value of your storage.
+                <p class="font-bold text-7xl w-[680px] my-10" style="line-height:80px;">
+                    Manage and share your files with friends.
                 </p>
-                <p class="w-[400px] mb-10">
-                    We offer a faster way of sharing your files and document to your multiple receiptent.
+                <p class="w-[450px] mb-10">
+                    We offer a faster way of sharing your files and document with your family and friends.
                     Plus you will be able to store them, its secured and safe.
                 </p> 
 
-                <div class="flex justify-between items-center w-[30vw]">
-                    <button class="text-black w-[150px] h-[50px] flex justify-center items-center bg-[#eba953] font-semibold" @click="open_verify_dialog">Get Started</button>
-                    <button class="w-[155px] h-[50px] flex justify-center items-center border-[1px] border-[#eba953] font-semibold text-[#eba953]"><i class="mr-2 icon pi pi-download"></i> Install app</button>
+                <div class="flex justify-between items-center w-[35vw]">
+                    <button class="rounded-[5px] text-black w-[150px] h-[50px] flex justify-center items-center bg-[#eba953] font-semibold" @click="open_verify_dialog">Get Started</button>
+                    <button class="rounded-[5px] w-[155px] h-[50px] flex justify-center items-center border-[1px] border-[#eba953] font-semibold text-[#eba953]"><i class="mr-2 icon pi pi-download"></i> Install app</button>
                 </div>
-                 <div class="flex justify-between items-center mt-8 w-[35vw]">
+                 <div class="flex justify-between items-center mt-8 w-[38vw]">
                     <div class="flex flex-col">
                         <p class="font-bold text-4xl text-[#e9972c]">3.0M</p>
                         <p class="mt-5 text-sm w-[200px]">
