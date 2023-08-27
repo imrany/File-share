4db809898560950c21ec6507940346b6886492f6
imrany <imranmat254@gmail.com>
2023-08-27 21:10:05 +0300

    updated mobile screen navigation

--- src/components/ui/MobileNav.vue
+++ src/components/ui/MobileNav.vue
@@ -9,9 +9,9 @@ const props=defineProps<{
 </script>
 <template>
     <div class="shadow-md text-slate-600 bg-white fixed top-0 left-0 right-0 z-20" id="nav-title">
-        <div class="flex px-10 py-5 items-center">
-            <i @click="router.back()" class="icon pi pi-arrow-left text-xl mr-3"></i>
-                <p class="text-xl text-slate-800">{{props.title}}</p>
+        <div class="flex pr-10 pl-5 py-5 items-center">
+            <i @click="router.back()" class="icon pi pi-arrow-left text-xl mr-6"></i>
+            <p class="text-xl text-slate-800">{{props.title}}</p>
         </div>
     </div>
 </template>
