6dd20b5f31fe0a2ec5c01b0444b06395c17bc268
imrany <imranmat254@gmail.com>
2023-10-20 13:37:13 +0300

    ui update

--- src/components/ui/Toast/type/FirstTutorials.vue
+++ src/components/ui/Toast/type/FirstTutorials.vue
@@ -11,17 +11,25 @@ const closeToast=()=>{
 }
 </script>
 <template>
-    <div v-if="route.fullPath==='/home'" class="fixed bottom-8 text-gray-700 bg-white max-sm:left-14 max-sm:right-2 sm:right-14 z-50 rounded-md shadow-md shadow-gray-500">
-        <div class="flex flex-col">
-            <button  class="ml-[auto] outline-none m-2" @click="closeToast">
-                <i class="icon pi pi-times text-sm hover:text-[#F45858]"></i>
-            </button>
-            <div class="pb-4 px-8">
-                <p class="text-xl font-semibold">Welcome to Wekafile</p>
-                <p class="text-sm">Get started for free and take leverage of our services.</p>
-                <div class="flex justify-between mt-4">
-                    <button @click="closeToast" class="text-gray-100 w-[90px] font-semibold text-sm h-[35px] rounded-[5px] transition-all shadow-sm flex justify-center items-center bg-gray-600">Close</button>
-                    <RouterLink to="/signin" class="text-gray-100 w-[90px] font-semibold text-sm h-[35px] rounded-[5px] transition-all shadow-sm flex justify-center items-center bg-green-400">Sign in</RouterLink>
+    <div class="flex flex-col justify-center items-center h-[100vh] w-full" v-if="route.fullPath==='/home'">
+        <div class="text-gray-700 bg-white sm:right-14 z-50 max-sm:mx-[10vw] rounded-md shadow-md shadow-gray-500">
+            <div class="flex flex-col">
+                <button  class="ml-[auto] outline-none m-2" @click="closeToast">
+                    <i class="icon pi pi-times text-sm hover:text-[#F45858]"></i>
+                </button>
+                <div class="pb-4 px-8 lg:w-[35vw] md:w-[45vw] sm:w-[55vw]">
+                    <p class="text-xl font-semibold">Welcome to Wekafile</p>
+                    <p class="text-sm mt-1">
+                        <span>
+                            Hello I'm <b>Marco</b>, your personal AI assistance. I'm here to help.
+                            I proceed with me step by step and know how to use Wekafile.
+                        </span>
+                        <span>In no time you would be a prop.</span>
+                    </p>
+                    <div class="flex justify-between mt-4">
+                        <button @click="closeToast" class="text-gray-600 border-[1px] w-[90px] font-semibold text-sm h-[35px] rounded-[5px] transition-all shadow-sm flex justify-center items-center">Close</button>
+                        <button class="text-gray-100 w-[90px] font-semibold text-sm h-[35px] rounded-[5px] transition-all shadow-sm flex justify-center items-center bg-green-400">Proceed</button>
+                    </div>
                 </div>
             </div>
         </div>
