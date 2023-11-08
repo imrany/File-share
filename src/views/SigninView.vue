ceb086d130d03a0ec97697f900139faf1178588d
imrany <imranmat254@gmail.com>
2023-11-08 10:20:36 +0300

    login ux update

--- src/views/SigninView.vue
+++ src/views/SigninView.vue
@@ -4,6 +4,7 @@ import { useRouter } from 'vue-router';
 import { useToast } from 'vue-toast-notification';
 
 const toast=useToast()
+const error=ref('')
 const router=useRouter()
 const email=ref("")
 const origin:any=inject("origin")
@@ -24,10 +25,7 @@ const handleSubmit=async(e:any)=>{
     e.preventDefault()
     try {
         if(password.value.length<7){
-            toast.info("Kindly, fill in the fields as required.",{
-                duration:3000,
-                position:"top-right"
-            }) 
+            error.value="Requires a minimum of 8 letters"
         }else if(password.value.length>7){
             isLoading.value=true
             wait.value="cursor-progress bg-gray-400"
@@ -52,6 +50,7 @@ const handleSubmit=async(e:any)=>{
                     duration:3000,
                     position:"top-right"
                 })
+                error.value=parseRes.error
                 isLoading.value=false
                 wait.value="cursor-pointer bg-[#e9972c]"
             }else{
@@ -78,12 +77,20 @@ const handleSubmit=async(e:any)=>{
 }
 
 const checkInput=()=>{
-    if(password.value.length<6&&!email.value){
+    if(password.value.length<7&&!email.value){
         isLoading.value=true
         wait.value="cursor-not-allowed"
-    }else if(password.value.length>6&&email.value){
+    }else if(password.value.length>7&&email.value){
         isLoading.value=false
         wait.value="cursor-pointer"
+    }
+}
+
+const checkPassword=()=>{
+    if(password.value.length>7&&password.value.length>0){
+        error.value=''
+    }else if(password.value.length<7&&password.value.length>0){
+        error.value="Requires a minimum of 8 letters"
     }
 }
 </script>
@@ -91,11 +98,11 @@ const checkInput=()=>{
     <div class="flex flex-col bg-[#fffbf7] justify-center items-center h-[100vh]">
         <div class="flex flex-col justify-center items-center md:w-[350px] max-md:w-[80vw] ">
             <p class="text-2xl font-semibold mb-4 text-gray-700">Sign in to <span class="text-[#e9972c]">Wekafile</span></p>
-            <form class="my-3 flex flex-col w-full" @submit="handleSubmit" @keydown="checkInput" >
+            <form class="my-3 flex flex-col w-full" @submit="handleSubmit" @keyup="checkInput" >
                 <label for="email" class="ml-1 flex justify-between max-md:text-sm"><span>Enter your email</span></label>
                 <input type="email" v-model="email" id="email" name="email" class="mt-2 h-[40px] border-gray-800 border-[1px] bg-white rounded-lg focus:outline-1 focus:outline-[#e9972c] py-2 px-4 placeholder:text-gray-900" placeholder="johndoe@example.com" required/>
-                <label for="password" class="ml-1 mt-4 flex justify-between max-md:text-sm"><span>Enter password</span> <span class="text-red-600 text-sm" v-if="password.length<8&&password.length>0">Requires a minimum of 8 letters</span></label>
-                <input type="password" v-model="password" id="password" name="password" class="mt-2 h-[40px] border-gray-800 border-[1px] bg-white rounded-lg focus:outline-1 focus:outline-[#e9972c] py-2 px-4 placeholder:text-gray-900" placeholder="Password" required/>
+                <label for="password" class="ml-1 mt-4 flex justify-between max-md:text-sm"><span>Enter password</span> <span class="text-red-600 text-sm">{{ error }}</span></label>
+                <input type="password" @keyup="checkPassword" :minlength="7" :maxlength="24" v-model="password" id="password" name="password" class="mt-2 h-[40px] border-gray-800 border-[1px] bg-white rounded-lg focus:outline-1 focus:outline-[#e9972c] py-2 px-4 placeholder:text-gray-900" placeholder="Password" required/>
                 <div class="flex justify-between gap-2 max-sm:text-sm">
                     <button type="button" @click="router.back()" class="font-semibold flex my-3 mt-6 justify-center items-center rounded-[50px] h-[40px] max-sm:w-[130px] w-[150px] border-[1px] border-gray-400 text-black">
                         Back
